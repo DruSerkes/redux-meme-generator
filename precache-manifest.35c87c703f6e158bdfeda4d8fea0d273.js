b96d608ac55d02cb66f6c51c887d3b2aebf6840b
Dru Serkes <DruSerkes@gmail.com>
2020-08-11 00:22:30 -0400

    Updates

---
+++ precache-manifest.35c87c703f6e158bdfeda4d8fea0d273.js
@@ -0,0 +1,26 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "ec7e7f537f21579b6af7e5e321305534",
+    "url": "/redux-meme-generator/index.html"
+  },
+  {
+    "revision": "38f36ede547bb6915ba8",
+    "url": "/redux-meme-generator/static/css/main.09515e9c.chunk.css"
+  },
+  {
+    "revision": "4c0844d7a376a2ad2551",
+    "url": "/redux-meme-generator/static/js/2.9cd73f5a.chunk.js"
+  },
+  {
+    "revision": "c64c486544348f10a6d6c716950bc223",
+    "url": "/redux-meme-generator/static/js/2.9cd73f5a.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "38f36ede547bb6915ba8",
+    "url": "/redux-meme-generator/static/js/main.ade7f308.chunk.js"
+  },
+  {
+    "revision": "cad5ba36fff6cf4af3ab",
+    "url": "/redux-meme-generator/static/js/runtime-main.3c3cecaf.js"
+  }
+]);
